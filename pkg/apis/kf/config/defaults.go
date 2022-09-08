99b2031c7565ba7ab000e9fd762bc9660093076d
Adrienne Galloway <adrienne.galloway@engineerbetter.com>
2022-09-08 15:57:45 +0100

    Create settings for TerminationGracePeriodSeconds
    Signed-off-by: Adrienne Galloway <adrienne.galloway@engineerbetter.com>
    Co-authored-by: Dipo Ajayi <ajayidipo@ymail.com>

--- pkg/apis/kf/config/defaults.go
+++ pkg/apis/kf/config/defaults.go
@@ -26,21 +26,22 @@ const (
 	// DefaultsConfigName is the name of the defaults configmap.
 	DefaultsConfigName = "config-defaults"
 
-	spaceContainerRegistryKey   = "spaceContainerRegistry"
-	spaceClusterDomainsKey      = "spaceClusterDomains"
-	spaceBuildpacksV2Key        = "spaceBuildpacksV2"
-	spaceStacksV2Key            = "spaceStacksV2"
-	spaceStacksV3Key            = "spaceStacksV3"
-	spaceDefaultToV3StackKey    = "spaceDefaultToV3Stack"
-	routeServiceProxyImageKey   = "routeServiceProxyImage"
-	featureFlagsKey             = "featureFlags"
-	buildDisableIstioSidecarKey = "buildDisableIstioSidecar"
-	buildPodResourcesKey        = "buildPodResources"
-	buildRetentionCountKey      = "buildRetentionCount"
-	buildTimeoutKey             = "buildTimeout"
-	buildNodeSelectorsKey       = "buildNodeSelectors"
-	appCPUPerGBOfRAMKey         = "appCPUPerGBOfRAM"
-	appCPUMinKey                = "appCPUMin"
+	spaceContainerRegistryKey        = "spaceContainerRegistry"
+	spaceClusterDomainsKey           = "spaceClusterDomains"
+	spaceBuildpacksV2Key             = "spaceBuildpacksV2"
+	spaceStacksV2Key                 = "spaceStacksV2"
+	spaceStacksV3Key                 = "spaceStacksV3"
+	spaceDefaultToV3StackKey         = "spaceDefaultToV3Stack"
+	routeServiceProxyImageKey        = "routeServiceProxyImage"
+	featureFlagsKey                  = "featureFlags"
+	buildDisableIstioSidecarKey      = "buildDisableIstioSidecar"
+	buildPodResourcesKey             = "buildPodResources"
+	buildRetentionCountKey           = "buildRetentionCount"
+	buildTimeoutKey                  = "buildTimeout"
+	buildNodeSelectorsKey            = "buildNodeSelectors"
+	appCPUPerGBOfRAMKey              = "appCPUPerGBOfRAM"
+	appCPUMinKey                     = "appCPUMin"
+	terminationGracePeriodSecondsKey = "terminationGracePeriodSeconds"
 
 	// Images used for build purposes
 
@@ -126,6 +127,12 @@ type DefaultsConfig struct {
 
 	// Minimum amount of CPU to assign an app.
 	AppCPUMin *resource.Quantity `json:"appCPUMin,omitempty"`
+
+	// Optional duration in seconds the pod needs to terminate gracefully.
+	// May be decreased in delete request. Value must be non-negative integer.
+	// The value zero indicates delete immediately. If this value is nil, the default grace period will be used instead.
+	// The grace period is the duration in seconds after the processes running in the pod are sent a termination signal and the time when the processes are forcibly halted with a kill signal. Set this value longer than the expected cleanup time for your process. Defaults to 30 seconds.
+	TerminationGracePeriodSeconds *int64 `json:"terminationGracePeriodSeconds,omitempty"`
 }
 
 // BuiltinDefaultsConfig creates a defaults configuration with default values.
@@ -223,13 +230,14 @@ func (defaultsConfig *DefaultsConfig) getStringValues() map[string]*string {
 // getInterfaceValues returns a map of the key/value pairs on a DefaultsConfig that are JSON/YAML encoded values.
 func (defaultsConfig *DefaultsConfig) getInterfaceValues(leaveEmpty bool) map[string]interface{} {
 	m := map[string]interface{}{
-		spaceDefaultToV3StackKey:    &defaultsConfig.SpaceDefaultToV3Stack,
-		buildDisableIstioSidecarKey: &defaultsConfig.BuildDisableIstioSidecar,
-		buildPodResourcesKey:        &defaultsConfig.BuildPodResources,
-		buildRetentionCountKey:      &defaultsConfig.BuildRetentionCount,
-		buildNodeSelectorsKey:       &defaultsConfig.BuildNodeSelectors,
-		appCPUPerGBOfRAMKey:         &defaultsConfig.AppCPUPerGBOfRAM,
-		appCPUMinKey:                &defaultsConfig.AppCPUMin,
+		spaceDefaultToV3StackKey:         &defaultsConfig.SpaceDefaultToV3Stack,
+		buildDisableIstioSidecarKey:      &defaultsConfig.BuildDisableIstioSidecar,
+		buildPodResourcesKey:             &defaultsConfig.BuildPodResources,
+		buildRetentionCountKey:           &defaultsConfig.BuildRetentionCount,
+		buildNodeSelectorsKey:            &defaultsConfig.BuildNodeSelectors,
+		appCPUPerGBOfRAMKey:              &defaultsConfig.AppCPUPerGBOfRAM,
+		appCPUMinKey:                     &defaultsConfig.AppCPUMin,
+		terminationGracePeriodSecondsKey: &defaultsConfig.TerminationGracePeriodSeconds,
 	}
 
 	if !leaveEmpty {
